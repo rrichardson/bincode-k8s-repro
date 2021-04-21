eb28a236674ad90431fd60fa0f029774a8c19a06
Rick Richardson <rick.richardson@gmail.com>
2021-04-21 09:39:44 -0700

    added ingress example

--- src/blobs.rs
+++ src/blobs.rs
@@ -75,3 +75,99 @@ pub static EXAMPLE1: &'static str = r#"
 }
 "#;
 
+
+pub static INGRESS : &'static str = r#"
+{
+    "apiVersion": "networking.k8s.io/v1beta1",
+    "kind": "Ingress",
+    "metadata": {
+        "annotations": {
+            "cert-manager.io/cluster-issuer": "letsencrypt-cf",
+            "kubectl.kubernetes.io/last-applied-configuration": "{\"apiVersion\":\"extensions/v1beta1\",\"kind\":\"Ingress\",\"metadata\":{\"annotations\":{\"cert-manager.io/cluster-issuer\":\"letsencrypt-cf\",\"kubernetes.io/ingress.class\":\"kong-internal\",\"kubernetes.io/tls-acme\":\"true\"},\"name\":\"grafana\",\"namespace\":\"grafana\"},\"spec\":{\"rules\":[{\"host\":\"grafana.test.me.local\",\"http\":{\"paths\":[{\"backend\":{\"serviceName\":\"grafana\",\"servicePort\":3000}}]}}],\"tls\":[{\"hosts\":[\"grafana.test.me.local\"],\"secretName\":\"grafana-tls\"}]}}\n",
+            "kubernetes.io/ingress.class": "kong-internal",
+            "kubernetes.io/tls-acme": "true"
+        },
+        "creationTimestamp": "2020-08-09T17:32:26Z",
+        "generation": 1,
+        "managedFields": [
+            {
+                "apiVersion": "extensions/v1beta1",
+                "fieldsType": "FieldsV1",
+                "fieldsV1": {
+                    "f:metadata": {
+                        "f:annotations": {
+                            ".": {},
+                            "f:cert-manager.io/cluster-issuer": {},
+                            "f:kubectl.kubernetes.io/last-applied-configuration": {},
+                            "f:kubernetes.io/ingress.class": {},
+                            "f:kubernetes.io/tls-acme": {}
+                        }
+                    },
+                    "f:spec": {
+                        "f:rules": {},
+                        "f:tls": {}
+                    }
+                },
+                "manager": "kubectl",
+                "operation": "Update",
+                "time": "2020-08-09T17:32:26Z"
+            },
+            {
+                "apiVersion": "networking.k8s.io/v1beta1",
+                "fieldsType": "FieldsV1",
+                "fieldsV1": {
+                    "f:status": {
+                        "f:loadBalancer": {
+                            "f:ingress": {}
+                        }
+                    }
+                },
+                "manager": "kong-ingress-controller",
+                "operation": "Update",
+                "time": "2020-08-09T17:32:55Z"
+            }
+        ],
+        "name": "grafana",
+        "namespace": "grafana",
+        "resourceVersion": "24602774",
+        "selfLink": "/apis/extensions/v1beta1/namespaces/grafana/ingresses/grafana",
+        "uid": "e19ed3c2-004f-4342-9818-f55ea5526dec"
+    },
+    "spec": {
+        "rules": [
+            {
+                "host": "grafana.test.me.local",
+                "http": {
+                    "paths": [
+                        {
+                            "backend": {
+                                "serviceName": "grafana",
+                                "servicePort": 3000
+                            },
+                            "pathType": "ImplementationSpecific"
+                        }
+                    ]
+                }
+            }
+        ],
+        "tls": [
+            {
+                "hosts": [
+                    "grafana.test.me.local"
+                ],
+                "secretName": "grafana-tls"
+            }
+        ]
+    },
+    "status": {
+        "loadBalancer": {
+            "ingress": [
+                {
+                    "hostname": "internal-aa1a0c0cb6679457bbe3f1c4a5e8f748-1899515855.us-east-1.elb.amazonaws.com"
+                }
+            ]
+        }
+    }
+}
+"#;
+
--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,35 @@
 use serde_json;
 use bincode::{Options, DefaultOptions};
 use k8s_openapi::api::core::v1::Endpoints;
+use k8s_openapi::api::networking::v1beta1::Ingress;
 mod blobs;
 
 fn main() {
+
     // Deserializes from JSON just fine
-    let ep : Endpoints = serde_json::from_str(blobs::EXAMPLE1).expect("unpack from json");
+    let ing : Ingress = serde_json::from_str(blobs::INGRESS).expect("unpack ingress from json");
 
     // To bincode bits
-    let blob = DefaultOptions::new()
+    let ing_bits = DefaultOptions::new()
+            .with_fixint_encoding()
+            .with_big_endian()
+            .serialize(&ing)
+            .expect("serialize with bincode");
+
+    // Deserializing from bincode fails
+    let bin_ing : Ingress = DefaultOptions::new()
+            .with_fixint_encoding()
+            .with_big_endian()
+            .deserialize(&ing_bits)
+            .expect("roundtrip back to struct");
+
+    println!("Ingress Result {:?}", bin_ing);
+
+    // Deserializes from JSON just fine
+    let ep : Endpoints = serde_json::from_str(blobs::EXAMPLE1).expect("unpack endpoint from json");
+
+    // To bincode bits
+    let endp_bits = DefaultOptions::new()
             .with_fixint_encoding()
             .with_big_endian()
             .serialize(&ep)
@@ -19,8 +40,8 @@ fn main() {
     let bin_ep : Endpoints = DefaultOptions::new()
             .with_fixint_encoding()
             .with_big_endian()
-            .deserialize(&blob)
+            .deserialize(&endp_bits)
             .expect("roundtrip back to struct");
 
-    println!("Result {:?}", bin_ep);
+    println!("Endpoints Result {:?}", bin_ep);
 }
